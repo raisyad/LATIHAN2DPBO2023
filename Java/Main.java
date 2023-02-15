55969e65234e15663d147e8016fa0151d6a2b65f
Syncz <92106283+raisyad@users.noreply.github.com>
2023-02-15 11:53:54 +0700

    Create Main.java

--- Java/Main.java
+++ Java/Main.java
@@ -2,15 +2,17 @@
 // Saya Raisyad Jullfikar NIM 2106238 mengerjakan Latihan 2 
 // dalam mata kuliah Desain dan Pemrograman Berorientasi Objek untuk 
 // keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
+// Import Library
 import java.util.*;
 
 public class Main {
   public static void main(String args[]) {
-    ArrayList<Mahasiswa> list = new ArrayList<>();
-    CR cr = new CR();
+    ArrayList<Mahasiswa> list = new ArrayList<>(); // Deklar arraylist untuk menampung objek kedalam var list
+    CR cr = new CR(); // Init var crud untuk objek crud
 
-    Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-    Boolean flag = true;
+    Scanner sc = new Scanner(System.in).useLocale(Locale.US); // Input / Pembuka Scanner
+    Boolean flag = true; // Penanda Loop
     int n = 0;
     do {
       System.out
@@ -21,14 +23,14 @@ public class Main {
         System.out.println("Inputan Bukan Pilihan Menu!");
       }
 
-      if (n == 1) {
+      if (n == 1) { // Menu 1 -> Create Data
         cr.Create(list);
-      } else if (n == 2) {
+      } else if (n == 2) { // Menu 2 -> Read Data
         cr.Read(list);
-      } else {
+      } else { // Selain menu diatas, loop berhenti
         flag = false;
       }
     } while (flag);
-    sc.close();
+    sc.close(); // Penutup Inputan
   }
 }
