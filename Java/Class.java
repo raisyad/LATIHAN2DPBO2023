94fa6fdfdcb69cbf57f25ce62c593782ad277099
Syncz <92106283+raisyad@users.noreply.github.com>
2023-02-15 11:50:06 +0700

    Update Class.java

--- Java/Class.java
+++ Java/Class.java
@@ -2,22 +2,26 @@
 // dalam mata kuliah Desain dan Pemrograman Berorientasi Objek untuk 
 // keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
 class Human {
+  // Private attr string
   private String NIK,
       name,
       jenis_kelamin;
 
+  // Construct kosong (default value)
   public Human() {
     this.NIK = "";
     this.name = "";
     this.jenis_kelamin = "";
   }
 
+  // Construct bervalue parameter
   public Human(String NIK, String name, String jenis_kelamin) {
     this.NIK = NIK;
     this.name = name;
     this.jenis_kelamin = jenis_kelamin;
   }
 
+  // Setter and getter of class Human
   public String getName() {
     return this.name;
   }
@@ -47,16 +51,19 @@ class SivitasAkademik extends Human {
   private String asal_universitas,
       email_edu;
 
+  // Construct kosong (default value)
   public SivitasAkademik() {
     this.asal_universitas = "";
     this.email_edu = "";
   }
 
+  // Construct bervalue parameter
   public SivitasAkademik(String asal_universitas, String email_edu) {
     this.asal_universitas = asal_universitas;
     this.email_edu = email_edu;
   }
 
+  // Setter and getter of class Sivitas Akademik
   public String getAUniv() {
     return this.asal_universitas;
   }
@@ -79,18 +86,21 @@ class Mahasiswa extends SivitasAkademik {
       fakultas,
       prodi;
 
+  // Construct kosong (default value)
   public Mahasiswa() {
     this.NIM = "";
     this.fakultas = "";
     this.prodi = "";
   }
 
+  // Construct bervalue parameter
   public Mahasiswa(String name, String NIM, String fakultas, String prodi) {
     this.NIM = NIM;
     this.fakultas = fakultas;
     this.prodi = prodi;
   }
 
+  // Setter and getter of class Mahasiswa
   public String getNim() {
     return this.NIM;
   }
