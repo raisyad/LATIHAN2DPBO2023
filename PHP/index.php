b98aed0c66083ba87a9d1054f969b95589016e28
Syncz <92106283+raisyad@users.noreply.github.com>
2023-02-20 21:08:11 +0700

    Update index.php

--- PHP/index.php
+++ PHP/index.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+// Saya Raisyad Jullfikar NIM 2106238 mengerjakan Latihan 2 
+// dalam mata kuliah Desain dan Pemrograman Berorientasi Objek untuk 
+// keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
 // Import file
   require('CR.php');
 
