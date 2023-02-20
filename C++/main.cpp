ea75f57d338b3dd5fb8cb92719187e6da6036b7b
Syncz <92106283+raisyad@users.noreply.github.com>
2023-02-20 21:06:06 +0700

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,3 +1,7 @@
+// Saya Raisyad Jullfikar NIM 2106238 mengerjakan Latihan 2 
+// dalam mata kuliah Desain dan Pemrograman Berorientasi Objek untuk 
+// keberkahanNya maka saya tidak melakukan kecurangan seperti yang telah dispesifikasikan. Aamiin.
+
 // Import Library
 #include <string>
 #include <iostream>
